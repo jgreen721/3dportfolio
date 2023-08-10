05b6d44005b16ee32273c6a86c8c3b0f11576916
Justin Green <justingreen@Justins-Air.lan>
2023-08-10 17:03:50 -0400

    save

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,28 +1,58 @@
-import React, {useEffect, useState} from 'react'
+import React, {useEffect, useState , useRef} from 'react'
 import "./Skills.css"
-import { SkillItem } from './components'
-import { Canvas } from '@react-three/fiber'
-import { OrbitControls, useGLTF, ScrollControls,Scroll } from '@react-three/drei'
+import { Canvas, useFrame , useThree } from '@react-three/fiber'
+import {  useGLTF } from '@react-three/drei'
+import { Raycaster,Vector3} from "three"
+import gsap from "gsap"
 
 
 
-const Icon = ({skill})=>{
-  const img = useGLTF(skill.logo)
+const Icon = ({ skill,mouseX, mouseY }) => {
+  const img = useGLTF(skill.logo);
+  const imgRef = useRef();
+  const dirLight = useRef();
+  const [posY,setPosY] = useState(2)
 
 
+const shineLight = ()=>{
+  console.log("shine light!")
+  dirLight.current.position.y = 3;
+  gsap.to(imgRef.current.position,{z:.5,duration:2})
 
-  return(
-    <group>
-      <directionalLight position={[2,2,2]}/>
-    <primitive  scale={skill.scale} position={skill.position} rotation={skill.rotation} object={img.scene}></primitive>
-    </group>
+}
 
-  )
+const removeLight=()=>{
+  console.log("remove light!")
+  dirLight.current.position.y = -100;
+  gsap.to(imgRef.current.position,{z:0,duration:2})
+
+
 }
 
 
-const Skills = () => {
 
+
+  return (
+    <group>
+      <directionalLight ref={dirLight} position={[2, -500, 2]} />
+      <primitive
+        onPointerEnter={shineLight}
+        onPointerLeave={removeLight}
+        ref={imgRef}
+        scale={skill.scale}
+        position={skill.position}
+        rotation={skill.rotation}
+        object={img.scene}
+      ></primitive>
+    </group>
+  );
+};
+
+
+
+const Skills = () => {
+  const [mouseX,setMouseX] = useState(0)
+  const [mouseY,setMouseY] = useState(0)
   const [skills,setSkills] = useState([
     {id:1,name:"HTML5",years:"5",logo:"html.glb",scale:.5,position:[-4,0,0],rotation:[0,0,0]},
     {id:2,name:"CSS3",years:"5",logo:"css.glb",scale:.5,position:[-.5,0,0],rotation:[0,0,0]},
@@ -30,9 +60,7 @@ const Skills = () => {
   ])
 
 
-  const htmlImg = useGLTF(skills[0].logo)
-  const cssImg = useGLTF(skills[1].logo)
-  const reactlImg = useGLTF(skills[2].logo)
+
 
   useEffect(()=>{
     if(innerWidth < 650){
@@ -50,13 +78,13 @@ setSkills(()=>[
              <Canvas>
          <color attach="background" args={["black"]}/>
          <ambientLight/>
-         <OrbitControls enableZoom={false}/>
+         {/* <OrbitControls enableZoom={false}/> */}
         {/* <SkillItem key={s.id} skill={s}/> */}
         {/* <ScrollControls pages={3}>
           <Scroll> */}
-        <Icon skill={skills[0]}/>
-        <Icon skill={skills[1]}/>
-        <Icon skill={skills[2]}/>
+        <Icon mouseX={mouseX} mouseY={mouseY} skill={skills[0]} changeY={.005}/>
+        <Icon mouseX={mouseX} mouseY={mouseY} skill={skills[1]} changeY={.008}/>
+        <Icon mouseX={mouseX} mouseY={mouseY} skill={skills[2]} changeY={.011}/>
         {/* </Scroll>
         </ScrollControls> */}
         {/* <primitive  scale={skills[0].scale} position={skills[0].position} rotation={skills[0].rotation} object={htmlImg.scene}></primitive>
